155a19afe3d544da10488fec6786546125d2743e
Fabiosal22 <fabiosalomaosales@gmail.com>
2024-11-22 19:35:26 -0300

    commit inicial

---
+++ src/Pages/Doados/Doados.jsx
@@ -0,0 +1,14 @@
+
+
+
+
+
+export default function Doados(){
+
+    return(
+        <section>
+            <h2>Aqui ficam os livos Doados</h2>
+        </section>
+    )
+}
+
---
+++ src/Pages/QueroDoar/QueroDoar.jsx
@@ -0,0 +1,13 @@
+
+
+
+export default function QueroDoar(){
+
+    return(
+        <section>
+            <h2>Quero Doar</h2>
+            <h3>aqui sai tudo na tela como se fosse uma sub main</h3>
+        </section>
+    )
+}
+
